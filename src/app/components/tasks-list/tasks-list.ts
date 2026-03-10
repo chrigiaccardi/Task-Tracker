be7183331c2405f2dd65fd00abe42fb85222f27f
Christian Giaccardi <christianerita@Mac-2.lan>
2026-03-10 07:52:55 +0100

    caricamento dati da backend local, ottimizzazione errore con asyncPipe

--- src/app/components/tasks-list/tasks-list.ts
+++ src/app/components/tasks-list/tasks-list.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { Task } from '../../interface/task';
 import { TaskItem } from '../task-item/task-item';
 import { TaskService } from '../../services/task-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tasks-list',
-  imports: [TaskItem],
+  imports: [TaskItem,AsyncPipe],
   templateUrl: './tasks-list.html',
   styleUrl: './tasks-list.css',
 })
-export class TasksList implements OnInit{
-  constructor(private taskService: TaskService){}
+export class TasksList {
+  // constructor(private taskService: TaskService, private cdr: ChangeDetectorRef) { }
+  taskService = inject(TaskService)
+  tasks1 = this.taskService.getTasks()
   tasks: Task[] = []
-  ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
-  }
+// ngOnInit(): void {
+//   this.taskService.getTasks().subscribe((tasks) => {
+//     this.tasks = tasks;
+//     console.log(this.tasks);
+//     this.cdr.detectChanges()
+//   });
+// }
 }
--- src/app/services/task-service.ts
+++ src/app/services/task-service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Task } from '../interface/task';
 import { TASKS } from '../mock-tasks';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5001/tasks'
+
+  constructor(private http: HttpClient){}
   
-  getTasks(): Observable <Task[]> {
-    const tasks = of(TASKS);
-    return tasks
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
